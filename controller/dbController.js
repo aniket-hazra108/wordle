54e225e4fffcd59506d7179e27659677014ff4ea
Ritwik Neema <ritwikneema651@gmail.com>
2022-03-06 23:07:19 +0530

    db connection

--- controller/dbController.js
+++ controller/dbController.js
@@ -1,17 +1,20 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@wordle.xcwww.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const testDb = function (req, res, next) {
-    client.connect(err => {
-        if (err) throw err;
-        console.log("Bhai connect ho gya!!!!");
-        const collection = client.db().collection("test");
-        client.close();
-        next();
+    client.connect(connectionErr => {
+        if (connectionErr) throw connectionErr;
+        const collection = client.db("WordleDB").collection("test");
+        collection.findOne({}, (fetchError, result) => {
+            if (fetchError) throw fetchError;
+            console.log(result);
+            client.close();
+            next();
+        })
     });
+
 }
 
 module.exports = testDb;
