e6ef60750ef63562fe2dc9035684d2b8cd1389c9
Ritwik Neema <ritwikneema651@gmail.com>
2022-03-06 13:15:54 +0530

    removed body parser

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 8080 ;
+const PORT = process.env.PORT || 8080;
 
 const gameController = require('./controller/gameController');
 const loginController = require('./controller/loginController');
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use("/registrar", loginController);
 app.use("/ingame", gameController);
 
-
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send("Hello, It is not valid");
 });
 
-
-
-app.listen(PORT, (err)=>{
-    if(err)
+app.listen(PORT, (err) => {
+    if (err)
         console.log(err)
     else
         console.log(PORT);
